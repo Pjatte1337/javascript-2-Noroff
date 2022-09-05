cb7932e665bf4daf7d51dbf10bc55d704a3456b1
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-09-05 15:38:04 +0200

    hiding api key

--- src/js/components/api/conn.js
+++ src/js/components/api/conn.js
@@ -4,7 +4,7 @@ const postUrl = `/api/v1/social/posts`;
 const options = {
  headers: {
   Authorization:
-   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwibmFtZSI6IkNocmlzdG9ubjkzIiwiZW1haWwiOiJDaHJUb24zMTg5OUBzdHVkLm5vcm9mZi5ubyIsImF2YXRhciI6InN0cmluZyIsImJhbm5lciI6InN0cmluZyIsImlhdCI6MTY2MjM3NDMyM30.ZWx_PjIMfcDd8m6Iz9EQqHzPW4wbGNAjydOqKcj2pkg",
+
  },
 };
 
