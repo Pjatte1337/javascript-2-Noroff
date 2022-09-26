096475e2eddb08283151d57c748f9baf97a1f191
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-09-26 09:14:26 +0200

    bugfix

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -2,24 +2,35 @@
 import * as user from "./user/index.mjs";
 import { logOut } from "./user/login/logout.mjs";
 import { displayProfile } from "./user/profile/profile.mjs";
+import { display404Page } from "./constant/404page.mjs";
+import { checkUserStatus } from "./constant/statcheck.mjs";
 
 // // Creating variables to use in the if statement under
 const logOutButton = document.querySelector("#logout");
-
-const index = document.getElementById("index");
-const profile = document.getElementById("profile");
-
-// // Executing sign in function if the location is based on the log in location
-if (index) {
-    user.formEvent()
-}
-
-// Executing the the function for the profile page layout
-if (profile) {
- displayProfile();
-}
 
 //Executing logout function if the logout button is present
 if (logOutButton) {
  logOut();
 }
+
+async function router() {
+ const routeName = document.body.id;
+
+ switch (routeName) {
+  // Executing sign in function if the location is based on the log in location
+  case "index":
+   user.formEvent();
+   checkUserStatus(localStorage.getItem("token"))
+   break;
+
+   // Executing the the function for the profile page layout
+  case "profile":
+   displayProfile();
+   break;
+
+  case "404":
+  default:
+   display404Page();
+ }
+}
+router()
