6167ce583f09ba467596f8b2d48996124ae89607
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-09-26 11:21:49 +0200

    fixing some layouts

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -5,6 +5,9 @@ import { displayProfile } from "./user/profile/profile.mjs";
 import { display404Page } from "./constant/404page.mjs";
 import { checkUserStatus } from "./constant/statcheck.mjs";
 
+// Importing layout
+import { generateFooter } from "./constant/layout/footer.js";
+
 // // Creating variables to use in the if statement under
 const logOutButton = document.querySelector("#logout");
 
@@ -20,17 +23,20 @@ async function router() {
   // Executing sign in function if the location is based on the log in location
   case "index":
    user.formEvent();
-   checkUserStatus(localStorage.getItem("token"))
+   checkUserStatus(localStorage.getItem("token"));
+   generateFooter();
    break;
 
-   // Executing the the function for the profile page layout
+  // Executing the the function for the profile page layout
   case "profile":
    displayProfile();
+   generateFooter();
    break;
 
   case "404":
   default:
    display404Page();
+   generateFooter();
  }
 }
-router()
+router();
