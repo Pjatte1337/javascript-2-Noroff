676e96705cfbd86662b1040a898e834d9fdfe01f
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-10-06 13:32:17 +0200

    Fixing delete post form

--- src/js/api/posts/deletePost.mjs
+++ src/js/api/posts/deletePost.mjs
@@ -7,7 +7,7 @@ let id = "";
 // Re-declaring variables from import
 const url = apiVar.baseURL;
 const postDetails = apiVar.getPostsById + id;
-const deletePost = apiVar.deletePost + id;
+const deletePost = apiVar.deletePost;
 
 export function deletePostListener(id) {
  document.querySelector(`button[id=deletePost-${id}]`).addEventListener("click", (e) => {
@@ -18,32 +18,58 @@ export function deletePostListener(id) {
 async function displayAlert(postId) {
  try {
   const request = await fetchApi(url + postDetails + postId, "GET", localStorage.getItem("token"), null);
-  const { title, date, id } = request;
+  const { title, id } = request;
 
   const main = document.querySelector("#confirm-action");
 
   const displayContent = document.createElement("div");
-  displayContent.innerHTML = `<form class="">
-            <h4>Delete post</h4>
-            <p>Are you sure you want to delete the post with title - ${title}?</p>
-            <button type="submit" class="btn btn-theme-bg-sec" name="confirm" value="yes">Yes</button>
-            <button type="submit" class="btn btn-theme-bg-sec" name="confirm" value="no">no</button>
-    </form>`;
+  displayContent.id = "actionForm";
+  displayContent.innerHTML = `<form class="container-fluid d-flex gap-5 mt-5 mb-5 justify-content-center" id="deletePost-${id}">
+  <div class="d-flex flex-column card p-5">
+   <div class="d-flex flex-column">
+      <h4 class="align-self-center">Delete post</h4>
+      <p class="align-self-center">Are you sure you want to delete the post with title - ${title}?</p>
+   </div>
+   <div class="d-flex gap-2 justify-content-center">
+      <button id="accept" class="btn btn-theme-bg-sec" name="confirm" value="yes">Yes</button>
+      <button id="reject" class="btn btn-theme-bg-sec" name="confirm" value="no">no</button>
+   </div>
+  </div>
+ </form>`;
 
   main.append(displayContent);
 
-  console.log("this is the id to delete", id);
-
-  document.querySelector("button[value=yes]").addEventListener("click", removePost(id));
+  if (displayContent) {
+   eventListener(id);
+  }
  } catch (error) {
   console.log(error);
+ }
+}
+
+function eventListener(id) {
+ const form = document.querySelector(`#deletePost-${id}`);
+ const accept = document.querySelector("#accept");
+ const reject = document.querySelector("#reject");
+ const formContainer = document.querySelector("#actionForm");
+
+ if (form) {
+  accept.addEventListener("click", (event) => {
+   event.preventDefault();
+   // send it to API
+   removePost(id);
+  });
+
+  reject.addEventListener("click", (action) => {
+   formContainer.remove();
+  });
  }
 }
 
 async function removePost(id) {
  try {
   const request = await fetchApi(url + deletePost + id, "DELETE", localStorage.getItem("token"), null);
-  const response = await request.json();
+  window.location.reload();
  } catch (error) {
   message("Error");
  }
