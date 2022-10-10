51f90d96ba33d102b83ca8b8ab0785183b3901d6
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-10-10 15:08:24 +0200

    Create post js

--- src/js/api/posts/createPost.mjs
+++ src/js/api/posts/createPost.mjs
@@ -1,3 +1,21 @@
-export async function createPosts(){
-    
+import { API_SOCIAL_URL } from "../../constant/variables.mjs";
+import { authFetch } from "../auth/fetch.mjs";
+
+const action = "/posts";
+const method = "post";
+
+
+
+export async function createPosts(postData) {
+    const createPostURL = API_SOCIAL_URL + action;
+
+
+    const response = await authFetch(createPostURL, {
+        method,
+        body: JSON.stringify(postData)
+    })
+
+    const post = await response.json();
+
+    console.log(post)
 }
