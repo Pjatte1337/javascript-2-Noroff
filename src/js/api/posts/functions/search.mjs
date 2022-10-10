ff6135a465f554cd71e0000728ac2873fbb65ecb
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-10-10 09:26:33 +0200

    trying to fix search

--- src/js/api/posts/functions/search.mjs
+++ src/js/api/posts/functions/search.mjs
@@ -1,2 +1,22 @@
+import { fetchApi } from "../../../constant/fetch.mjs";
+import * as apiVar from "../../../constant/variables.mjs";
+
+// Retrieving items from storage
+const token = localStorage.getItem("token");
+
+// Re-declaring variables from import
+const url = apiVar.baseURL;
+const endpointPosts = apiVar.getPosts;
+
+const fetchUrl = url + endpointPosts;
+
+async function postFeed() {
+ try {
+  let request = await fetchApi(fetchUrl, "GET", token, null);
+ } catch (err) {
+  console.log("There was a problem retrieving the user posts", err);
+ }
+}
+
 // Selection HTML elements
 const searchInput = document.getElementById("postSearch");
