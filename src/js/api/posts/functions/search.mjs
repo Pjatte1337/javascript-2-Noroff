134abe36018b729114609215d953affece52af93
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-10-10 10:39:03 +0200

    working on search

--- src/js/api/posts/functions/search.mjs
+++ src/js/api/posts/functions/search.mjs
@@ -1,20 +1,35 @@
 import { fetchApi } from "../../../constant/fetch.mjs";
 import * as apiVar from "../../../constant/variables.mjs";
-
 // Retrieving items from storage
 const token = localStorage.getItem("token");
 
 // Re-declaring variables from import
 const url = apiVar.baseURL;
 const endpointPosts = apiVar.getPosts;
-
 const fetchUrl = url + endpointPosts;
 
-// Selection HTML elements
-const searchInput = document.getElementById("postSearch");
+const searchInput = document.querySelector("[data-search]");
+
+let posts = [];
+
+searchInput.addEventListener("input", (e) => {
+ const value = e.target.value.toLowerCase();
+ posts.forEach((i) => {
+  const allCards = document.querySelectorAll(`#post-id-${i.postId}`)
+  const isVisible = i.title.includes(value) || i.author.includes(value);
+        allCards.classList.toggle("d-none", !isVisible)
+ });
+});
 
 async function postFeed() {
- let request = await fetchApi(fetchUrl, "GET", token, null);
- const values = request;
-
+ try {
+  let request = await fetchApi(fetchUrl, "GET", token, null);
+  console.log(request)
+  posts = request.map((e) => {
+   return { title: e.title, body: e.body, author: e.author.name, postId: e.id };
+  });
+ } catch (err) {
+  console.log("There was a problem retrieving the user posts", err);
+ }
 }
+postFeed();
