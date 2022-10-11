de40ced594bfc3ebcb16ce136923e323e208edda
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-10-11 19:28:35 +0200

    refactoring functions

--- src/js/api/posts/postFeed.mjs
+++ src/js/api/posts/postFeed.mjs
@@ -19,7 +19,7 @@ export async function postFeed() {
  *
  * @param {*} postData This is needed to be able to populate the feed of posts.
  */
-export async function createPostFeed(postData) {
+export function createPostFeed(postData) {
   postData.forEach((data) => {
     const feedContainer = document.querySelector("#post-feed");
 
@@ -61,88 +61,98 @@ export async function createPostFeed(postData) {
       commentsHtml = comments
         .map(
           (e) => `
-       <div class="d-flex flex-column p-2"> 
-       <h5>Comments</h5>
-       <div class="container card me-1 p-1" id="commentId-${e.id}">
-         <div class="card-body">
-             <p class="card-text">${e.body}</p>
+         <div class="d-flex flex-column p-2"> 
+         <h5>Comments</h5>
+         <div class="container card me-1 p-1" id="commentId-${e.id}">
+           <div class="card-body">
+               <p class="card-text">${e.body}</p>
+           </div>
+           <div class="card-footer">
+               <small class="text-muted"> - ${e.owner}</small>
+               <div class="row">
+                 <small class="text-muted">Published ${commentsTimeCreated}</small>
+               </div>
+         </div> 
          </div>
-         <div class="card-footer">
-             <small class="text-muted"> - ${e.owner}</small>
-             <div class="row">
-               <small class="text-muted">Published ${commentsTimeCreated}</small>
-             </div>
-       </div> 
-       </div>
-     `
+       `
         )
         .join("");
     }
 
+    // Looking for author image. Displaying image if the return value is true
     if (authorAvatar) {
       userAvatar = `<img src="${authorAvatar}" class="img-thumbnail user-avatar-small" alt="" loading="lazy" />`;
     }
 
+    // Looking for author.name in the api fetch. Displaying settings wheel if the return value is true
     const currentUser = localStorage.getItem("username");
     if (currentUser === authorName) {
       postSettings = `<span class="settings d-flex justify-content-end">
-     <div class="dropdown">
-     <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-gear"></i></a>
-     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-       <li><button class="dropdown-item change-post" id="updatePost-id-${id}">Update</button></li>
-       <li><button class="dropdown-item change-post" id="deletePost-id-${id}">Delete</button></li>
-     </ul>
-   </div>`;
+       <div class="dropdown">
+       <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-gear"></i></a>
+       <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+         <li><button class="dropdown-item change-post" id="updatePost-id-${id}">Update</button></li>
+         <li><button class="dropdown-item change-post" id="deletePost-id-${id}">Delete</button></li>
+       </ul>
+     </div>`;
     }
 
-    const card = new LoopingCard(
-      "div",
-      {
-        id: `post-id-${id}`,
-        class: "card container-fluid d-flex justify-content-center gap-3 p-0",
-        "data-id": "postItem",
-      },
-      `<div class="card-header">
-       <div class="d-flex flex-fill">
-        <div class="d-flex flex-fill gap-2 align-items-center">
-         ${userAvatar}
-         <h4 class="text-muted"><a href="" class="muted-link text-muted">${authorName}</a></h4>
-        </div>
-        ${postSettings}
-       </div>
-      </div>
-    
-      <div class="card-body">
-       <a href="../posts/index.html?id=${id}" class="h5 text-black text-decoration-none"><h5 class="card-title">${title}</h5></a>
-       <p class="card-text">${body}.</p>
-       ${postImage}
-      </div>
-      <div class="card-footer">
-       <div class="row">
-        <small class="text-muted">Published ${formattedCreated}</small>
-        <small class="text-muted">Last updated ${formattedUpdated}</small>
-       </div>
-       <div class="mt-2">
-        <button class="btn" id="btn-comments"><i class="fa-regular fa-comment"></i> ${count.comments}</button>
-        <button class="btn" id="btn-like"><i class="fa-solid fa-thumbs-up"></i>  ${count.reactions}</button>
-       </div>
-      </div>
-     </div>
-     <div class="d-none">
-      <div id="comments-${id}">
-       <form action="" class="card p-2 mb-5">
-        <div class="container">
-         <div class="mb-3 gap-1">
-          <textarea class="form-control"></textarea>
-          <button class="btn float-end" type="submit">Comment</button>
-         </div>
-        </div>
-       </form>
-      </div>
-      ${commentsHtml}
-     </div>
-    </div>`
-    );
+    // Choosing what type of HTML element to render
+    const classElement = "div";
+
+    // Adding attributes to the HTML element
+    const classAttributes = {
+      id: `post-id-${id}`,
+      class: "card container-fluid d-flex justify-content-center gap-3 p-0",
+      "data-id": "postItem",
+    };
+
+    // Laying out the HTMl to render for each card
+    const classTemplate = `<div class="card-header">
+<div class="d-flex flex-fill">
+ <div class="d-flex flex-fill gap-2 align-items-center">
+  ${userAvatar}
+  <h4 class="text-muted"><a href="" class="muted-link text-muted">${authorName}</a></h4>
+ </div>
+ ${postSettings}
+</div>
+</div>
+
+<div class="card-body">
+<a href="../posts/index.html?id=${id}" class="h5 text-black text-decoration-none"><h5 class="card-title">${title}</h5></a>
+<p class="card-text">${body}.</p>
+${postImage}
+</div>
+<div class="card-footer">
+<div class="row">
+ <small class="text-muted">Published ${formattedCreated}</small>
+ <small class="text-muted">Last updated ${formattedUpdated}</small>
+</div>
+<div class="mt-2">
+ <button class="btn" id="btn-comments"><i class="fa-regular fa-comment"></i> ${count.comments}</button>
+ <button class="btn" id="btn-like"><i class="fa-solid fa-thumbs-up"></i>  ${count.reactions}</button>
+</div>
+</div>
+</div>
+<div class="d-none">
+<div id="comments-${id}">
+<form action="" class="card p-2 mb-5">
+ <div class="container">
+  <div class="mb-3 gap-1">
+   <textarea class="form-control"></textarea>
+   <button class="btn float-end" type="submit">Comment</button>
+  </div>
+ </div>
+</form>
+</div>
+${commentsHtml}
+</div>
+</div>
+  `;
+
+    // Creating a new card based on variables defined over.
+    const card = new LoopingCard(classElement, classAttributes, classTemplate);
+
     // Removing loader
     const loader = document.querySelector(".loader");
     loader.style = "display: none;";
