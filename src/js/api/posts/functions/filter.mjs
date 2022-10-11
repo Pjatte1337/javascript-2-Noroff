efc74d5aab9bcf69d85bdbf35a09c82909a4a645
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-10-11 14:57:34 +0200

    Update filter.mjs

--- src/js/api/posts/functions/filter.mjs
+++ src/js/api/posts/functions/filter.mjs
@@ -5,7 +5,6 @@ export function filterButtonListener() {
   allButtons.forEach(function (btn) {
     btn.addEventListener("click", (e) => {
       const category = e.currentTarget.dataset.filter;
-      // btn.classList.add("btn-theme-secondary")
       filteringData(category);
     });
   });
@@ -23,26 +22,18 @@ async function filteringData(value) {
 
     case "all":
       sortedArray = newArray;
-      //  console.log("ok all");
-      //  console.log("New array all", newArray);
       break;
 
     case "date":
       sortedArray = newArray.sort(sortByDate);
-      //  console.log("ok date");
-      //  console.log("New array date", newArray);
       break;
 
     case "A_zSort":
       sortedArray = newArray.sort(sortByAtoZ);
-      //  console.log("ok a-z");
-      //  console.log("New array A_zSort", newArray);
       break;
 
     case "Z_aSort":
       sortedArray = newArray.sort(sortByZtoA);
-      //  console.log("ok z-a");
-      //  console.log("New array Z_aSort", newArray);
       break;
   }
 }
